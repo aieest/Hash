82b817eae6fcff9fa05a2e3a00362a54949e0d43
August Estidola <aie.estidola@gmail.com>
2023-09-15 15:52:28 +0800

    Hash 2: Multi-nested hash (not yet done)

---
+++ hash_2.rb
@@ -0,0 +1,49 @@
+employee_data = {
+  "Engineering Department" => {
+    "employee1" => {
+      "name" => "John Doe",
+      "age" => 30,
+      "position" => "Software Engineer",
+      "skills" => ["Ruby", "Python", "JavaScript"],
+      "contact" => {
+        "email" => "john@example.com",
+        "phone" => "+123456789"
+      }
+    },
+    "employee2" => {
+      "name" => "Jane Smith",
+      "age" => 25,
+      "position" => "Front-end Developer",
+      "skills" => ["HTML", "CSS", "JavaScript"],
+      "contact" => {
+        "email" => "jane@example.com",
+        "phone" => "+987654321"
+      }
+    }
+  },
+  "Marketing Department" => {
+    "employee3" => {
+      "name" => "David Johnson",
+      "age" => 28,
+      "position" => "Marketing Specialist",
+      "skills" => ["Social Media", "SEO", "Content Marketing"],
+      "contact" => {
+        "email" => "david@example.com",
+        "phone" => "+567890123"
+      }
+    },
+    "employee4" => {
+      "name" => "Emily White",
+      "age" => 32,
+      "position" => "Marketing Manager",
+      "skills" => ["Market Research", "Campaign Management", "Analytics"],
+      "contact" => {
+        "email" => "emily@example.com",
+        "phone" => "+345678901"
+      }
+    }
+  }
+}
+
+#Look for the employees with the age of 28 and 30
+
